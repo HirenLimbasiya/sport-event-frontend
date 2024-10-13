3dd398859bd8f40e1f4ab2e76e73de037f2d43ae
Hiren <hiren@Hirens-MacBook-Air.local>
2024-10-13 21:49:09 +0530

    update env

--- src/App.js
+++ src/App.js
@@ -1,58 +1,75 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Events from './pages/Events';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Events from "./pages/Events";
 import CreateEvent from "./pages/CreateEvent";
 import MyEvents from "./pages/MyEvents";
 import Participate from "./pages/Participant";
 import Pending from "./pages/Pending";
-import PrivateComponents from './components/PrivateComponents';
-import { useState } from 'react';
-import EventPage from './pages/EventPage';
-
+import PrivateComponents from "./components/PrivateComponents";
+import { useState } from "react";
+import EventPage from "./pages/EventPage";
 
 function App() {
-
   const [myEvents, setMyEvents] = useState([]);
   const [pending, setPending] = useState([]);
   const [participate, setParticipate] = useState([]);
 
   const fetchMyEvents = async (id) => {
-    const response = await fetch(`http://localhost:5000/myevents/${id}`, {
-      method: "GET",
-      headers: {
-        authorization: `${JSON.parse(localStorage.getItem('token'))}`
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/myevents/${id}`,
+      {
+        method: "GET",
+        headers: {
+          authorization: `${JSON.parse(localStorage.getItem("token"))}`,
+        },
       }
-    })
+    );
     const data = await response.json();
     setMyEvents(data.myEvents);
     setParticipate(data.participated);
-    setPending(data.pending)
-    console.log(data)
-  }
-
-
+    setPending(data.pending);
+    console.log(data);
+  };
 
   function eventFunction(id) {
-    fetchMyEvents(id)
+    fetchMyEvents(id);
   }
 
   return (
     <>
       <Header />
       <Routes>
-        <Route element={<PrivateComponents />} >
-          <Route path='/' element={<Events eventFunction={eventFunction} />} />
-          <Route path='/createevent' element={<CreateEvent />} />
-          <Route path='/myevents' element={<MyEvents myEvents={myEvents} eventFunction={eventFunction} />} />
-          <Route path='/participate' element={<Participate participate={participate} eventFunction={eventFunction} />} />
-          <Route path='/pendingevent' element={<Pending pending={pending} eventFunction={eventFunction} />} />
-          <Route path='/myevents/:id' element={<EventPage />} />
+        <Route element={<PrivateComponents />}>
+          <Route path="/" element={<Events eventFunction={eventFunction} />} />
+          <Route path="/createevent" element={<CreateEvent />} />
+          <Route
+            path="/myevents"
+            element={
+              <MyEvents myEvents={myEvents} eventFunction={eventFunction} />
+            }
+          />
+          <Route
+            path="/participate"
+            element={
+              <Participate
+                participate={participate}
+                eventFunction={eventFunction}
+              />
+            }
+          />
+          <Route
+            path="/pendingevent"
+            element={
+              <Pending pending={pending} eventFunction={eventFunction} />
+            }
+          />
+          <Route path="/myevents/:id" element={<EventPage />} />
         </Route>
-        <Route path='/signup' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,65 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const local = localStorage.getItem("users");
-        if (local) {
-            navigate("/")
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    async function handleLogin(e) {
-        e.preventDefault();
-
-        if (password.trim() === "" || username.trim() === "") {
-            alert("Please Enter a data");
-            return
-        }
-        const response = await fetch("http://localhost:5000/login", {
-            method: "post",
-            body: JSON.stringify({ username, password }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        const data = await response.json();
-        if (!data.error) {
-            localStorage.setItem("users", JSON.stringify({ name: data.user.username, id: data.user._id }));
-            localStorage.setItem("token", JSON.stringify(data.auth));
-            navigate("/")
-        } else {
-            alert("Username or Password are invalid")
-        }
-
+  useEffect(() => {
+    const local = localStorage.getItem("users");
+    if (local) {
+      navigate("/");
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <section>
-            <h1 className='heading'>Login</h1>
-            <form className='form-lr' onSubmit={handleLogin}>
-                <label>Username : </label><br />
-                <input type="text"
-                    placeholder="Enter Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)} /><br />
-                <label>Password : </label><br />
-                <input type="password"
-                    placeholder="Enter Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} /><br />
+  async function handleLogin(e) {
+    e.preventDefault();
 
-                <button type='submit' className='btn-lr'>Login</button>
-            </form>
-        </section>
-    )
-}
+    if (password.trim() === "" || username.trim() === "") {
+      alert("Please Enter a data");
+      return;
+    }
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/login`, {
+      method: "post",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    if (!data.error) {
+      localStorage.setItem(
+        "users",
+        JSON.stringify({ name: data.user.username, id: data.user._id })
+      );
+      localStorage.setItem("token", JSON.stringify(data.auth));
+      navigate("/");
+    } else {
+      alert("Username or Password are invalid");
+    }
+  }
 
-export default Login
+  return (
+    <section>
+      <h1 className="heading">Login</h1>
+      <form className="form-lr" onSubmit={handleLogin}>
+        <label>Username : </label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <label>Password : </label>
+        <br />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+
+        <button type="submit" className="btn-lr">
+          Login
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
